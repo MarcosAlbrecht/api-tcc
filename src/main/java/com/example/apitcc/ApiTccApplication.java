b4ea4eff59b5753735d91f0c363bee96296e0b83
unknown <marquinhos.beto@hotmail.com>
2021-09-21 09:02:54 -0300

    commit para adicionar todos os arquivos do projeto

---
+++ src/main/java/com/example/apitcc/ApiTccApplication.java
@@ -0,0 +1,13 @@
+package com.example.apitcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTccApplication.class, args);
+	}
+
+}
